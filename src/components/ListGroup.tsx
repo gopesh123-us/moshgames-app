ef9f5386456c969a82fb4ae857457b5e101ff617
Gopesh Sharma <gopesh.sharma@gmail.com>
2023-08-24 22:32:49 -0400

    useState for active class

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,28 @@
-
+import { MouseEvent, useState } from "react";
 
 function ListGroup() {
   const cities = ["New York", "London", "Paris", "New Delhi", "San Francisco"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
+  /*  const handleClick = (event: MouseEvent) => {
+    console.log(event);
+  };
+ */
   return (
     <>
       <h1>List Group</h1>
+      {cities.length === 0 && <p>No cities found</p>}
       <ul className="list-group">
-        {cities.map((city) => (
-          <li className="list-group-item" key={city}>
+        {cities.map((city, index) => (
+          <li
+            className={
+              selectedIndex == index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={city}
+            onClick={() => setSelectedIndex(index)}
+          >
             {city}
           </li>
         ))}
