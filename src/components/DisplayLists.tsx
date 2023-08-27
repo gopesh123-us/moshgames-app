e3fe4e0e5ed8bd0246d95149a9a50b6f7ac1bcc1
Gopesh Sharma <gopesh.sharma@gmail.com>
2023-08-27 08:58:57 -0400

    DisplayLists

--- src/components/DisplayLists.tsx
+++ src/components/DisplayLists.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 interface DisplayListsProps {
   list: string[];
   heading: string;
 }
 function DisplayLists({ list, heading }: DisplayListsProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
-      <h1>{heading}</h1>
+      <h1 className="display-1">{heading}</h1>
       <ul className="list-group">
-        {list.map((item) => (
-          <li className="list-group-item">{item}</li>
+        {list.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
